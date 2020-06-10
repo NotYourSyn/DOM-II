68c3b0b3bb5470d586575db9aafca275aa306cc0
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-06-09 20:50:27 -0400

    halfway point

--- README.md
+++ README.md
@@ -29,21 +29,21 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 * [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
+* [x] Once you see the red screen, you can delete that style and you're ready to start on the next task
 
 ## Task 2: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
+	* [x] `mouseover`
 	* [ ] `keydown`
 	* [ ] `wheel`
 	* [ ] `drag / drop`
-	* [ ] `load`
+	* [x] `load`
 	* [ ] `focus`
 	* [ ] `resize`
 	* [ ] `scroll`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [x] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+
+// mouseover / mouseleave on h2's
+const head2 = document.querySelectorAll('h2');
+head2.forEach((item) =>{
+    item.addEventListener('mouseover', (e) =>{
+        e.target.style.color = 'red';
+    });
+    item.addEventListener('mouseleave', (e) =>{
+        e.target.style.color = 'black';
+    });
+});
+
+// load page notification 
+window.addEventListener('load', (e) =>{
+    alert('Welcome To Fun Bus');
+});
+
+// click on header to enlarge
+const head1 = document.querySelectorAll('h1');
+head1.forEach((item) =>{
+    item.addEventListener('click', (e) =>{
+        e.target.style.fontSize = '7rem';
+    });
+    item.addEventListener('mouseleave', (e) =>{
+        e.target.style.fontSize ='4rem';
+    });
+});
+
+// double click on img to enlarge 
+
+const growImg = document.querySelectorAll('img');
+growImg.forEach((item) =>{
+    item.addEventListener('dblclick', (e) =>{
+        e.target.style.transform = 'scale(2)';
+        e.target.style.transition = '5s';
+    });
+    item.addEventListener('mouseleave', (e) =>{
+        e.target.style.transform = 'scale(1)';
+    })
+});
